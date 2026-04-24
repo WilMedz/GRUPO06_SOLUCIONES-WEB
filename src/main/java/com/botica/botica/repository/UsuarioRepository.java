88bb1e5ab75711af4f5f510898e9efc278038859
JCarlosCCH <wolfgand28@gmail.com>
2026-04-24 01:50:33 -0500

    Update UsuarioRepository.java

--- src/main/java/com/botica/botica/repository/UsuarioRepository.java
+++ src/main/java/com/botica/botica/repository/UsuarioRepository.java
@@ -3,10 +3,7 @@ package com.botica.botica.repository;
 import com.botica.botica.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
-    Optional<Usuario> findByEmail(String email);
 }
