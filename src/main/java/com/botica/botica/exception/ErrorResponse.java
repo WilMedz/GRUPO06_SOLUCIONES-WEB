b4735885b1da36c22538f1775cd15aedd0a86bd5
WilMedz <wilmamediza22@gmail.com>
2026-04-23 23:40:12 -0500

    creamos un traductor para capturar errores y devolver mensaje

--- src/main/java/com/botica/botica/exception/ErrorResponse.java
+++ src/main/java/com/botica/botica/exception/ErrorResponse.java
@@ -1,5 +1,20 @@
 package com.botica.botica.exception;
 
-public class ErrorResponse {
+import java.time.LocalDateTime;
+import java.util.Map;
+import lombok.Data;
 
+@Data
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+    private Map<String, String> errors; // Aquí guardaremos qué campo falló (ej: "stock")
+
+    public ErrorResponse(int status, String message, Map<String, String> errors) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
 }
